2f1af3d603c2f0062641bbc27355025747785727
gc2004 <55647897+undersquire@users.noreply.github.com>
2023-01-10 23:44:12 -0800

    Formatted with rustfmt.

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,9 @@
-
-
-
 /// To use with AEAD symmetric algorithms.
 pub trait Aead {
     /// Cryptographic nonce.
     type Nonce;
 
-    fn encrypt(
-        &mut self, 
-        key: &Self::Nonce,
-        paintext: &[u8],
-    ) -> Box<[u8]>;
+    fn encrypt(&mut self, key: &Self::Nonce, paintext: &[u8]) -> Box<[u8]>;
 
-    fn decrypt(
-        &mut self,
-        key: &Self::Nonce,
-        ciphertext: &[u8],
-    ) -> Box<[u8]>;
+    fn decrypt(&mut self, key: &Self::Nonce, ciphertext: &[u8]) -> Box<[u8]>;
 }
